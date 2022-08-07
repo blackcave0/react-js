2deea04b2999eb06a92620694214b300d80714ff
blackcave09 <i3intel@protonmail.com>
2022-08-08 04:27:52 +0530

    m

---
+++ src/slotMachine/C28_gamePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const C28_gamePage = () => {
+    return ( 
+        <>
+        </>
+    )
+}
+
+export default C28_gamePage
---
+++ src/slotMachine/SlotApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const SlotApp = () => {
+    return ( 
+        <>
+
+        </>
+    )
+}
+export default SlotApp
